9c135ceecf909140f3223cce86752d5bbf5bafbb
Lakori4 <santivalde04@gmail.com>
2026-03-22 15:06:15 +0100

    added behaviour to display and hide the inner container

--- src/app/components/ContainerBackHome.tsx
+++ src/app/components/ContainerBackHome.tsx
@@ -1,19 +1,26 @@
 "use client"
 import { useRouter } from "next/navigation";
 import "./ContainerBackHome.css";
+import { useState } from "react";
 
 
 const ContainerBackHome = ({children} : {children?: React.ReactNode}) => {
 
     const router = useRouter();
 
+    const [showInner, setShowInner] = useState<boolean>(false)
+
     return(
-        <div className="container">
-            <h1>Our little component</h1>
-            <button onClick={() => { router.push("/")}}>Home</button>
-            <div>
-                {children}
-            </div>
+        <div onClick={() => {showInner ? setShowInner(false): null}} className="container">
+           {/* <h1>This is a title in the outer container</h1> 
+            <button onClick={() => { router.push("/")}}>Home</button>*/}
+            
+            {!showInner && <button onClick={() => {setShowInner(true); console.log(showInner)}}>Display lorem ipsum</button>}
+            {showInner && 
+                <div onClick={(e) => {e.stopPropagation()}} className="innerContainer">
+                    {children}
+                </div>
+            }
         </div>
     )
 }
