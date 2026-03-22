8d05ee86ff3798db3576170f78ba1e186bfed60a
Lakori4 <santivalde04@gmail.com>
2026-03-22 15:04:59 +0100

    added lorem ipsum text to innerContainer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ const Home = () => {
     <div>
       <h1>Our little page</h1>
       <ContainerBackHome>
-        <p>Howdy</p>        
+        <h1>This is a Title inside the container</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem consectetur est ducimus dolorem aut, quaerat numquam reprehenderit cumque accusamus, provident et voluptatem itaque accusantium? Consectetur laborum saepe dicta eum maxime!</p>
       </ContainerBackHome>
     </div>
   )
